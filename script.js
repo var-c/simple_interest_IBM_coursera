b31a5ad2f9d7f4a740a7b83a122147633c5d72b9
var-c <89406389+var-c@users.noreply.github.com>
2021-08-24 10:34:02 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,13 @@
+function compute()
+{
+    p = document.getElementById("principal").value;
+    i = document.getElementById("interest").value;
+    t = document.getElementById("years").value;
+
+    var final = (p*i*t)/100
+
+    var finalll = "The final value is " + final
+
+    document.getElementById("output").innerHTML=finalll;
+}
+        
